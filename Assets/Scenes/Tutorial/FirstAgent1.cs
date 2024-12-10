937c0f57bf2d86838103389e51835e072b105544
casperjarmer <casper@jarmer.dk>
2024-12-10 12:22:58 +0100

    Example implemented

---
+++ Assets/Scenes/Tutorial/FirstAgent1.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class FirstAgent : Agent
+{
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private Material winMaterial;
+    [SerializeField] private Material loseMaterial;
+    [SerializeField] private MeshRenderer floorMeshRenderer;
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = Vector3.zero;
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTransform.localPosition);
+    }
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
+
+        float moveSpeed = 2f;
+        transform.localPosition += new Vector3(moveX,0,moveZ)*Time.deltaTime * moveSpeed;
+    }
+
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = -Input.GetAxisRaw("Vertical");
+        continuousActions[1] = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Goal"))
+        {
+            SetReward(+1f);
+            floorMeshRenderer.material = winMaterial;
+            EndEpisode();
+        }
+        if (other.TryGetComponent<Wall>(out Wall wall))
+        {
+            SetReward(-1f);
+            floorMeshRenderer.material = loseMaterial;
+            EndEpisode();
+        }
+
+    }
+
+}
